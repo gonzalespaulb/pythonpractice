4aff17a5b5da6b2bfb469baa92652756b89915b9
gonzalespaulb <76889345+gonzalespaulb@users.noreply.github.com>
2024-03-25 09:58:03 -0600

    decoder

--- 100days/day8_caesar_cypher.py
+++ 100days/day8_caesar_cypher.py
@@ -2,9 +2,8 @@ import string
 
 all_letters = list(string.ascii_lowercase)
 
-word_to_encode = "zzz"
-
-shift_number = 2
+word_to_encode = input("What word to encode?\n")
+shift_number = int(input("How many shifts?\n"))
 
 # print(all_letters[26 - 1])
 
@@ -17,7 +16,6 @@ def encoder (word):
         index_in_alphabet = all_letters.index(word_as_list[i])
 
         # Take into account the wrap around
-        
         shifted_letter = all_letters[(index_in_alphabet + shift_number) % 26]
 
         word_as_list[i] = shifted_letter
@@ -26,9 +24,27 @@ def encoder (word):
 
     return encoded_word
 
-word_to_encode = encoder(word_to_encode)
-
-print(word_to_encode)
-
-
 # Also the decode
+
+def decoder (word): 
+
+    word_as_list = list(word.lower())
+
+    for i in range(len(word_as_list)): 
+
+        index_in_alphabet = all_letters.index(word_as_list[i])
+
+        # Take into account the wrap around
+        shifted_letter = all_letters[(index_in_alphabet - shift_number) % 26]
+
+        word_as_list[i] = shifted_letter
+
+    decoded_word = ''.join(word_as_list)
+
+    return decoded_word
+
+encoded_word = encoder(word_to_encode)
+print(encoded_word)
+print(decoder(encoded_word))
+
+
