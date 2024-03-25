f1a086d8114423782f9e79d9cb7449dc6f047d5f
gonzalespaulb <76889345+gonzalespaulb@users.noreply.github.com>
2024-03-25 17:22:40 -0600

    refactored

--- 100days/day8_caesar_cypher.py
+++ 100days/day8_caesar_cypher.py
@@ -2,13 +2,11 @@ import string
 
 all_letters = list(string.ascii_lowercase)
 
-word_to_encode = input("What word to encode?\n")
+word_to_encode = input("Type a word\n")
+encode_or_decode = input("Encode or decode?\n")
 shift_number = int(input("How many shifts?\n"))
 
-# print(all_letters[26 - 1])
-
-def encoder (word): 
-
+def caesar (word, decode_or_encode): 
     word_as_list = list(word.lower())
 
     for i in range(len(word_as_list)): 
@@ -16,35 +14,14 @@ def encoder (word):
         index_in_alphabet = all_letters.index(word_as_list[i])
 
         # Take into account the wrap around
-        shifted_letter = all_letters[(index_in_alphabet + shift_number) % 26]
+        shift_letter_back = all_letters[(index_in_alphabet - shift_number) % 26]
+        shift_letter = all_letters[(index_in_alphabet + shift_number) % 26]
 
-        word_as_list[i] = shifted_letter
+        word_as_list[i] = shift_letter if decode_or_encode == "encode" else shift_letter_back
 
-    encoded_word = ''.join(word_as_list)
+    encoded_or_decoded_word = ''.join(word_as_list)
 
-    return encoded_word
+    return encoded_or_decoded_word
 
-# Also the decode
-
-def decoder (word): 
-
-    word_as_list = list(word.lower())
-
-    for i in range(len(word_as_list)): 
-
-        index_in_alphabet = all_letters.index(word_as_list[i])
-
-        # Take into account the wrap around
-        shifted_letter = all_letters[(index_in_alphabet - shift_number) % 26]
-
-        word_as_list[i] = shifted_letter
-
-    decoded_word = ''.join(word_as_list)
-
-    return decoded_word
-
-encoded_word = encoder(word_to_encode)
-print(encoded_word)
-print(decoder(encoded_word))
-
-
+encoded_or_decoded_word = caesar(word_to_encode, encode_or_decode)
+print(encoded_or_decoded_word)
