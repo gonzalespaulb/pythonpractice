27bf176dbbc86926e04e757a9f7a0ecb84495bbf
gonzalespaulb <76889345+gonzalespaulb@users.noreply.github.com>
2024-03-30 21:31:23 -0600

    black jack done

--- 100days/day11_blackjack.py
+++ 100days/day11_blackjack.py
@@ -1,60 +1,157 @@
 import random
+import os
+import time
 
-starting_chips = 5000
+chips = 5000
 bet = 0
+
+# Function to clear the terminal screen
+def clear_screen():
+    # Clear screen command for Windows
+    if os.name == 'nt':
+        os.system('cls')
+    # Clear screen command for Unix/Linux/MacOS
+    else:
+        os.system('clear')
+
+def delay_timer(seconds): 
+    print("Game resetting in 5s.")
+    time.sleep(5)
+    clear_screen()
 
 def deal_cards(): 
     return random.randint(1, 11)
 
+def is21(card_1, card_2): 
+    if card_1 + card_2 == 21: 
+        return True
+    else: 
+        return False
+    
+def is_greater_than_17(cards): 
+    if sum(cards) >= 17: 
+        return True
+    
+    else: 
+        return False
+
 while True: 
 
-    print(f"You currently have ${starting_chips}")
+    if chips < 1:
+        reup = input("Looks like you're out of chips! Re up? y or n\n").lower()
 
-    player_card_1 = deal_cards()
-    player_card_2 = deal_cards()
-    dealer_card_1 = deal_cards()
-    dealer_card_2 = deal_cards()
+        if reup == "y": 
+            chips += 5000
+            print("Heres another $5000. On the house ;)\n")
+        
+        else: 
+            break
 
-    player_cards = [player_card_1, player_card_2]
-    dealer_cards = [dealer_card_1, dealer_card_2]
+    play_question = input("Would you like to play a hand? y or n\n").lower()
 
-    play = input("Would you like to play blackjack? y or n\n").lower()
-
-    if play == "y": 
-        set_bet = int(input("How much would you like to bet?\n"))
-        bet = set_bet
-        starting_chips -= set_bet
-        print(f"Player: [{player_card_1},{player_card_2}] Dealer: [{dealer_card_1}, *] ---- You currently have {player_card_1 + player_card_2}")
-
+    if play_question == "y": 
         while True: 
+            
+            bet = int(input(f"How much would you like to bet? You currently have ${chips}\n"))
 
-            hit_or_stay = input("Hit or stay?\n").lower()
-
-            # What happens if dealer has 21
-            if sum(deal_cards) == 21: 
-                print("Dealer has blackjack! You lost.")
-                break
-
-            # What happens on hit
-            if hit_or_stay == "hit": 
-                player_cards += deal_cards()
+            if bet > chips: 
+                print(f"You only have ${chips}. Please try again.\n")
                 continue
+                
+            else:
+                player_card_1 = deal_cards()
+                player_card_2 = deal_cards()
+                dealer_card_1 = deal_cards()
+                dealer_card_2 = deal_cards()
 
-            if hit_or_stay == "stay": 
-                if sum(dealer_cards) < 16: 
-                    dealer_cards += deal_cards()
-                    continue
+                player_cards = [player_card_1, player_card_2]
+                dealer_cards = [dealer_card_1, dealer_card_2]
+
+                print("Dealer is checking for 21.\n")
+
+                if is21(player_card_1, player_card_2): 
+                    chips += bet
+                    print(f"You have blackjack!! (+${bet})")
+                    print("Game resetting in 5s.")
+                    time.sleep(5)
+                    clear_screen()
+                    break
+
+                elif is21(dealer_card_1, dealer_card_2): 
+                    chips -= bet
+                    print(f"Dealer has blackjack!! (-${bet})")
+                    print("Game resetting in 5s.")
+                    time.sleep(5)
+                    clear_screen()
+                    break
+                
                 else: 
-                    if sum(dealer_cards) > sum(player_cards): 
-                        print(f"Dealer has {sum(deal_cards)} and you have {sum(player_cards)}. Dealer won!")
-                        break
-                    else: 
-                        starting_chips += bet * 2
-                        print(f"Dealer has {sum(deal_cards)} and you have {sum(player_cards)}. You won ${starting_chips}!")
-                        
-            # What happens on stay
-                # If dealer is less than 15 hit
-                # Else stay and compare with dealer cards
+                    initial_cards = f"Player-[{player_card_1}, {player_card_2}]: {player_card_1 + player_card_2} || Dealer-[{dealer_card_1}, *]\n"
+                    print(initial_cards)
 
+                    while True: 
+                        hit_or_stay = input(f"You currently have {sum(player_cards)}. Hit or stay? h or s?\n").lower()
+
+                        if hit_or_stay == "h": 
+                            player_cards.append(deal_cards())
+
+                            if sum(player_cards) > 21: 
+                                print(f"{sum(player_cards)}. You bust! (-${bet})")
+                                chips -= bet
+                                print("Game resetting in 5s.")
+                                time.sleep(5)
+                                clear_screen()
+                                break
+
+                            else: 
+                                continue
+
+                        elif hit_or_stay == "s":
+
+                            while True: 
+
+                                print(f"Dealer currently has {sum(dealer_cards)}.")
+
+                                if sum(dealer_cards) >= 17 and sum(dealer_cards) <= 21:
+                                    if sum(dealer_cards) > sum(player_cards): 
+                                        print(f"You lost! (-${bet})")
+                                        chips -= bet
+                                        print("Game resetting in 5s.")
+                                        time.sleep(5)
+                                        clear_screen()
+                                        break
+
+                                    elif sum(dealer_cards) == sum(player_cards): 
+                                        print("Its a push!")
+                                        print("Game resetting in 5s.")
+                                        time.sleep(5)
+                                        clear_screen()
+                                        break
+
+                                    else:
+                                        print(f"You win! (+${bet})")
+                                        chips += bet
+                                        print("Game resetting in 5s.")
+                                        time.sleep(5)
+                                        clear_screen()
+                                        break
+                                
+                                elif sum(dealer_cards) < 17: 
+                                    dealer_cards.append(deal_cards())
+                                    continue
+
+                                elif sum(dealer_cards) > 21: 
+                                    print(f"Dealer bust! You win! (+${bet})")
+                                    chips += bet
+                                    print("Game resetting in 5s.")
+                                    time.sleep(5)
+                                    clear_screen()
+                                    break
+                            break
+
+                        else: 
+                            print("Not a valid move.\n")
+                            continue
+            break                    
     else: 
-        print(f"Thank you for playing! Your total winnings are ${starting_chips}")
+        break
