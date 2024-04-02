3687cf9b247657e34e0e4479f12e583e65bd7e10
gonzalespaulb <76889345+gonzalespaulb@users.noreply.github.com>
2024-04-01 19:16:26 -0600

    data

---
+++ 100days/day14_higher_lower.py
@@ -0,0 +1,21 @@
+# Higher or lower game
+
+# Create a dictionary with the key and value
+
+# Create random matchups 
+
+# Have a loop with an input for which one is higher or lower
+
+country_sizes = {
+    "Russia": 17098242, 
+    "Canada": 9984670,
+    "China": 9706961, 
+    "USA": 9372610, 
+    "Brazil": 8515767,
+    "Australia": 7692024,
+    "India": 3287590,
+    "Argentina": 2780400, 
+    "Kazakhstan": 2724900, 
+    "Algeria":2381741,
+}
+
