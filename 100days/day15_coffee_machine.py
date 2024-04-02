a85b541e835643f8fea2a096f70970010189997d
gonzalespaulb <76889345+gonzalespaulb@users.noreply.github.com>
2024-04-02 10:09:10 -0600

    starting code

---
+++ 100days/day15_coffee_machine.py
@@ -0,0 +1,38 @@
+# Machine max resource capacity
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+# Function for resource deduction
+
+# Function for resource addition
+
+# Dictionary for recipes and cost
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+# Translate money into numbers
