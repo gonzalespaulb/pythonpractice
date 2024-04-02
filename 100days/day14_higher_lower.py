1ea0b0d21024a5ac0f75950c56e0af7ec7b50c49
gonzalespaulb <76889345+gonzalespaulb@users.noreply.github.com>
2024-04-01 19:49:24 -0600

    higher or lower

--- 100days/day14_higher_lower.py
+++ 100days/day14_higher_lower.py
@@ -6,6 +6,8 @@
 
 # Have a loop with an input for which one is higher or lower
 
+import random
+
 country_sizes = {
     "Russia": 17098242, 
     "Canada": 9984670,
@@ -19,3 +21,36 @@ country_sizes = {
     "Algeria":2381741,
 }
 
+country_keys = []
+
+for countries in country_sizes: 
+    country_keys.append(countries)
+
+score = 0
+lives = 5
+
+while True: 
+
+    country_1 = country_keys[random.randint(0, len(country_keys) - 1)]
+    country_2 = country_keys[random.randint(0, len(country_keys) - 1)]
+
+    if country_1 == country_2: 
+        continue
+
+    correct_country = (country_1 if country_sizes[country_1] > country_sizes[country_2] else country_2).lower()
+
+    which_country = input(f"Which country has a bigger landmass? {country_1} or {country_2}\n").lower()
+
+    if which_country == correct_country: 
+        score += 1
+        print("That's correct!")
+        continue
+
+    elif which_country != correct_country and lives > 0: 
+        lives -= 1
+        print("Wrong answer")
+        continue
+
+    else: 
+        print(f"Total score is {score}")
+        break
