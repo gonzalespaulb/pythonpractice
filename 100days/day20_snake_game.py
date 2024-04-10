28087bd69e972427519d875b6335d85b1bf87cef
gonzalespaulb <76889345+gonzalespaulb@users.noreply.github.com>
2024-04-10 14:23:25 -0600

    snake is moveable

--- 100days/day20_snake_game.py
+++ 100days/day20_snake_game.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import time
 
 screen = Screen()
 
@@ -8,6 +9,8 @@ HEIGHT = 600
 screen.setup(width=WIDTH, height=HEIGHT)
 screen.bgcolor("black")
 screen.title("Paulies Snake Game")
+screen.tracer(0)
+screen.listen()
 
 dots_eaten = ["_","_","_","_","_","_","_","_",]
 snake_length = []
@@ -20,25 +23,31 @@ for index in range(0, len(dots_eaten)):
     snake_length.append(t)
 
 
-# direction = "w"
-
-# def move_forward(): 
-#     t.forward(20)
-# def move_left(): 
-#     t.left(20)
-# def move_right(): 
-#     t.right(20)
-# def move_back(): 
-#     t.backward(20)
-
-# screen.onkey(key="w", fun=move_forward)
-# screen.onkey(key="a", fun=move_left)
-# screen.onkey(key="d", fun=move_right)
-# screen.onkey(key="s", fun=move_back)
 
 while True: 
-    for segment in snake_length: 
 
-        segment.forward(20)
+    screen.update()
+    time.sleep(0.1)
+    for seg_num in range(len(snake_length) - 1, 0, -1): 
+        new_x = snake_length[seg_num - 1].xcor()
+        new_y = snake_length[seg_num - 1].ycor()
+        snake_length[seg_num].goto(new_x, new_y)
+
+    def move_up(): 
+        snake_length[0].setheading(90)
+    def move_down(): 
+        snake_length[0].setheading(270)
+    def move_left(): 
+        snake_length[0].setheading(180)
+    def move_right(): 
+        snake_length[0].setheading(0)
+
+    screen.onkey(key="Up", fun=move_up)
+    screen.onkey(key="Down", fun=move_down)
+    screen.onkey(key="Left", fun=move_left)
+    screen.onkey(key="Right", fun=move_right)
+
+    snake_length[0].forward(20)
+
 
 screen.exitonclick()
