012480ab3c1c4c47c663d56a02065f72eb7aeaa2
gonzalespaulb <76889345+gonzalespaulb@users.noreply.github.com>
2024-04-13 23:26:36 -0600

    removed exponential bug

--- 100days/day20_snake_game.py
+++ 100days/day20_snake_game.py
@@ -1,10 +1,15 @@
 from turtle import Turtle, Screen
 import time
+import random
 
 screen = Screen()
 
 WIDTH = 600
 HEIGHT = 600
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 screen.setup(width=WIDTH, height=HEIGHT)
 screen.bgcolor("black")
@@ -12,42 +17,100 @@ screen.title("Paulies Snake Game")
 screen.tracer(0)
 screen.listen()
 
-dots_eaten = ["_","_","_","_","_","_","_","_",]
+dots_eaten = 2
 snake_length = []
 
-for index in range(0, len(dots_eaten)):
-    t = Turtle(shape="square")
-    t.penup()
-    t.color("white")
-    t.goto(index * -20, 0)
-    snake_length.append(t)
 
+def render_length(): 
+    for index in range(0, dots_eaten):
+        t = Turtle(shape="square")
+        t.penup()
+        t.color("white")
+        t.goto(index * -20, 0)
+        snake_length.append(t)
 
+render_length()
+
+snake_head = snake_length[0]
+
+def get_divisible_coordinate():
+  """Generates a random integer divisible by 20 within the specified range."""
+  while True:
+    value = random.randint(-290, 290)
+    if value % 20 == 0:
+      return value
+
+food = Turtle(shape="square")
+food.penup()
+food.color("white")
+food.goto(get_divisible_coordinate(), get_divisible_coordinate())
 
 while True: 
 
     screen.update()
     time.sleep(0.1)
+
+    # Start from the end of the list and assign the coords of the segment before to the current segment
     for seg_num in range(len(snake_length) - 1, 0, -1): 
         new_x = snake_length[seg_num - 1].xcor()
         new_y = snake_length[seg_num - 1].ycor()
         snake_length[seg_num].goto(new_x, new_y)
 
+    # If statement is to prevent from going backwards on itself
     def move_up(): 
-        snake_length[0].setheading(90)
+        if snake_head.heading() != DOWN:
+            snake_head.setheading(UP)
     def move_down(): 
-        snake_length[0].setheading(270)
+        if snake_head.heading() != UP:
+            snake_head.setheading(DOWN)
     def move_left(): 
-        snake_length[0].setheading(180)
+        if snake_head.heading() != RIGHT:
+            snake_head.setheading(LEFT)
     def move_right(): 
-        snake_length[0].setheading(0)
+        if snake_head.heading() != LEFT:
+            snake_head.setheading(RIGHT)
 
     screen.onkey(key="Up", fun=move_up)
     screen.onkey(key="Down", fun=move_down)
     screen.onkey(key="Left", fun=move_left)
     screen.onkey(key="Right", fun=move_right)
 
-    snake_length[0].forward(20)
+    if snake_head.distance(food) < 15: 
+        dots_eaten += 1
+        food.goto(get_divisible_coordinate(), get_divisible_coordinate())
 
+        t = Turtle(shape="square")
+        t.penup()
+        t.color("white")
+        snake_length.append(t)
+
+    snake_head.forward(20)
 
 screen.exitonclick()
+
+
+# class Animal: 
+#     def __init__(self): 
+#         self.num_eyes = 2
+
+#     def breathe(self): 
+#         print("Inhale, Exxhale")
+
+# class Fish(Animal): 
+#     def __init__(self): 
+#         super().__init__()
+
+#     # Takes an existing class' and add more
+#     def breathe(self):
+#         super().breathe()
+#         print("doing it underwater")
+
+#     def swim(self): 
+#         print("Swimming")
+
+# paul = Animal()
+# paul.breathe()
+
+# nemo = Fish()
+# nemo.breathe()
+# print(nemo.num_eyes)
