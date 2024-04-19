517daebd908613784657045bc4387ebdf561f318
gonzalespaulb <76889345+gonzalespaulb@users.noreply.github.com>
2024-04-19 15:00:00 -0600

    bounce off paddle

--- 100days/day22_pong.py
+++ 100days/day22_pong.py
@@ -1,10 +1,44 @@
 from turtle import Turtle, Screen
+import time
 
 HEIGHT = 600
 WIDTH = 800
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(height=HEIGHT, width=WIDTH) 
+
+
+# Ball Class
+class Ball(Turtle): 
+    def __init__(self): 
+        super().__init__()
+        self.shape("circle")
+        self.fillcolor("white")
+        self.penup()
+        self.x_move = 10
+        self.y_move = 10
+
+    def bounce(self): 
+        self.y_move *= -1 
+
+    def paddle_collide(self): 
+        self.x_move *= -1         
+
+    def move_right(self): 
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+
+    def move_left(self): 
+        new_x = self.xcor() - self.x_move
+        new_y = self.ycor() - self.y_move
+        self.goto(new_x, new_y)
+
+    def move(self): 
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)        
+
  
 #  Paddle class
 class Paddle(Turtle):
@@ -30,6 +64,7 @@ class Paddle(Turtle):
 
 user_paddle = Paddle(position=(350, 0))
 computer_paddle = Paddle(position=(-350, 0))
+ball = Ball()
 
 screen.listen()
 screen.onkey(user_paddle.go_up, "Up")
@@ -38,8 +73,21 @@ screen.onkey(computer_paddle.go_up, "w")
 screen.onkey(computer_paddle.go_down, "s")
 
 game_is_on = True
+ball_direction = "right"
 
 while game_is_on: 
+    time.sleep(0.05)
     screen.update()
+    ball.move()
+
+    if ball.distance(user_paddle) < 50 and ball.xcor() > 330:
+        ball.paddle_collide() 
+    
+    if ball.distance(computer_paddle) < 50 and ball.xcor() < -330:
+        ball.paddle_collide() 
+
+    if ball.ycor() > 280 or ball.ycor() < -280: 
+        ball.bounce()   
+
 
 screen.exitonclick()
