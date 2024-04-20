13c627ce901e4d19336fd6457b50eb9991501c87
gonzalespaulb <76889345+gonzalespaulb@users.noreply.github.com>
2024-04-20 13:03:40 -0600

    turtle class created

---
+++ 100days/day23_turtle_crossing.py
@@ -0,0 +1,27 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+WIDTH = 600 
+HEIGHT = 600
+
+screen.setup(width=WIDTH, height=HEIGHT)
+screen.tracer(0)
+
+class Player(Turtle): 
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.setheading(90)
+        self.fillcolor("green")
+        self.penup()
+        self.goto(0, -280)
+
+paulie = Player()
+
+game_is_on = True
+
+while game_is_on: 
+    screen.update()
+
+
+screen.exitonclick()
