2694cf85b4a17bfeeecbe0493ecc36c7c7dcf23f
gonzalespaulb <76889345+gonzalespaulb@users.noreply.github.com>
2024-04-20 13:09:08 -0600

    turtle moves

--- 100days/day23_turtle_crossing.py
+++ 100days/day23_turtle_crossing.py
@@ -16,7 +16,17 @@ class Player(Turtle):
         self.penup()
         self.goto(0, -280)
 
+    def move_forward(self): 
+        self.forward(20)
+
+    def move_backward(self): 
+        self.backward(20)    
+
 paulie = Player()
+
+screen.listen()
+screen.onkey(paulie.move_forward, "Up")
+screen.onkey(paulie.move_backward, "Down")
 
 game_is_on = True
 
