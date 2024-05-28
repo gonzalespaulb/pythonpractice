d948335a63f89ddde53552f952a9975ea7f9cd79
gonzalespaulb <76889345+gonzalespaulb@users.noreply.github.com>
2024-05-28 15:58:33 -0600

    being printed

--- day36_stock_market.py
+++ day36_stock_market.py
@@ -1,37 +1,40 @@
-news_api = "65abf95bb23e4f65aec8bac029e6abc0"
+import requests
+import time
+
 stock_api = "UQAJK7J49CZV1AYS"
 
-import requests
-
-# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-stock_url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey={stock_api}'
-stock_r = requests.get(stock_url)
-stock_data = stock_r.json()
-
-company_name = input("Enter a company name.\n").lower()
-news_url = f'https://newsapi.org/v2/everything?q=%22{company_name}%22&apiKey=65abf95bb23e4f65aec8bac029e6abc0'
+company_ticker = input("Enter a company ticker.\n").upper()
+news_url = f'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers={company_ticker}&apikey={stock_api}'
 news_r = requests.get(news_url)
 news_data = news_r.json()
 
-# print(news_data["status"])
+stock_url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={company_ticker}&interval=5min&apikey={stock_api}'
+stock_r = requests.get(stock_url)
+stock_data = stock_r.json()
 
-all_news_articles = news_data["articles"]
-all_descriptions = []
 
-for i in range(len(all_news_articles)): 
-    all_descriptions.append(all_news_articles[i]["title"])
-    # all_descriptions.append(all_news_articles[i]["content"])
+for day in stock_data["Time Series (Daily)"]: 
 
-for description in all_descriptions: 
-    print(f"{description}\n")
+    time.sleep(0.5)
+    
+    day_data = stock_data["Time Series (Daily)"][day]
+    split_date = day.split("-")
+    formatted_date = "".join(split_date)
+    relevant_news = []
+  
+    for news in news_data["feed"]: 
+        formatted_news_date = news["time_published"].split("T")[0]
 
-# Ask user for the company name
-# replace q keyword with company name in the news url api
+        if formatted_date == formatted_news_date: 
+            relevant_news.append(f"Title: {news['title']}\nSource: {news['source']}\nUrl: {news['url']}\n")
 
-# If company exists and theres new present it else tell user theres no news found
+    print(f"-----------{day}-----------\n")
+    print(f'Open: {day_data["1. open"]}')
+    print(f'Close: {day_data["4. close"]}')
+    print(f'High: {day_data["2. high"]}')
+    print(f'Low: {day_data["3. low"]}\n ')
+    print("Relevant News\n")
+    for news in relevant_news: 
+        print(news)
 
-# Check for news regarding  company
-
-# Pull up descriptions tied to the said company 
-
-# Cross reference publish day and stock trading info for that day
+    print("------------------------------\n")
