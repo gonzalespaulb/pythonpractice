6e5126aa669a9e48d2636c513013684f358972a2
gonzalespaulb <76889345+gonzalespaulb@users.noreply.github.com>
2024-05-30 10:00:47 -0600

    fuck this stupid api

--- day36_stock_market.py
+++ day36_stock_market.py
@@ -12,10 +12,14 @@ stock_url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbo
 stock_r = requests.get(stock_url)
 stock_data = stock_r.json()
 
+print(news_r.status_code)
+print(stock_r.status_code)
+
+data_to_render = {}
+
+
 
 for day in stock_data["Time Series (Daily)"]: 
-
-    time.sleep(0.5)
     
     day_data = stock_data["Time Series (Daily)"][day]
     split_date = day.split("-")
@@ -28,13 +32,48 @@ for day in stock_data["Time Series (Daily)"]:
         if formatted_date == formatted_news_date: 
             relevant_news.append(f"Title: {news['title']}\nSource: {news['source']}\nUrl: {news['url']}\n")
 
-    print(f"-----------{day}-----------\n")
-    print(f'Open: {day_data["1. open"]}')
-    print(f'Close: {day_data["4. close"]}')
-    print(f'High: {day_data["2. high"]}')
-    print(f'Low: {day_data["3. low"]}\n ')
-    print("Relevant News\n")
-    for news in relevant_news: 
-        print(news)
 
-    print("------------------------------\n")
+    data_to_render[day] = {
+        'prices': {
+            'open': day_data["1. open"],
+            'close': day_data["4. close"],
+            'high': day_data["2. high"],
+            'low': day_data["3. low"],
+        }, 
+        'relevant news': relevant_news
+    }
+
+    # print(f"-----------{day}-----------\n")
+    # print(f'Open: {day_data["1. open"]}')
+    # print(f'Close: {day_data["4. close"]}')
+    # print(f'High: {day_data["2. high"]}')
+    # print(f'Low: {day_data["3. low"]}\n ')
+    # print("Relevant News\n")
+    # for news in relevant_news: 
+    #     if formatted_date == formatted_news_date: 
+    #         print(news)
+
+    # print("------------------------------\n")
+
+
+    
+
+for  day in data_to_render: 
+    prices = data_to_render[day]['prices']
+    news = data_to_render[day]['relevant news']
+
+    if len(news) > 0:
+
+        print(f"-----------{day}-----------\n")
+        print(f'Open: {prices["open"]}')
+        print(f'Close: {prices["close"]}')
+        print(f'High: {prices["high"]}')
+        print(f'Low: {prices["low"]}\n ')      
+
+        print("Relevant News\n")
+
+        for article in news: 
+                print(article)
+
+        print(f"--------------------------------\n")  
+        
